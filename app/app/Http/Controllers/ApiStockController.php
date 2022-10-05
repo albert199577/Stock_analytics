3610fa2216920c722eea1b783ca2980836681d57
albert199577 <mso729049@gmail.com>
2022-10-05 11:38:27 +0800

    feat: add get few stock data api

---
+++ app/app/Http/Controllers/ApiStockController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Services\StockService as ServicesStockService;
+use App\Models\Stock;
+
+use Illuminate\Http\Request;
+
+class ApiStockController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    private $stockservice;
+    
+    public function __construct()
+    {
+        $this->stockservice = new ServicesStockService;
+    }
+
+    
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Stock  $stock
+     * @return \Illuminate\Http\Response
+     */
+    public function show($stock_id)
+    {
+        $data = $this->stockservice->getFewStocksData($stock_id);
+        return $data;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Stock  $stock
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Stock $stock)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Stock  $stock
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Stock $stock)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Stock  $stock
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Stock $stock)
+    {
+        //
+    }
+}
